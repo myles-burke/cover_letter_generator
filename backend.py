6a1bd7de7593235107a66facca7ef941376f005d
Myles Burke <burkemyles63@gmail.com>
2025-12-29 22:03:55 -0500

    major changes need to add main logic now

--- backend.py
+++ backend.py
@@ -1,27 +1,44 @@
-# pip install flask werkzeug
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, render_template, jsonify
 import os
 from werkzeug.utils import secure_filename
+import shutil
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
+KEY = os.getenv("APIKEY")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "data")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
+@app.route("/")
+def index():
+    return render_template("main.html")
+
 @app.route("/upload", methods=["POST"])
 def upload():
-    resume = request.files.get("resume")
-    job = request.files.get("job")
+    print("Active")
+    try:
+        resume = request.files.get("resume")
+        job = request.files.get("job")
 
-    if not resume or not job or resume.filename == "" or job.filename == "":
-        return {"success": False, "error": "Missing file"}, 400
+        if not resume or not job or resume.filename == "" or job.filename == "":
+            return jsonify(success=False, error="Missing file"), 400
 
-    resume.save(os.path.join(UPLOAD_FOLDER, secure_filename(resume.filename)))
-    job.save(os.path.join(UPLOAD_FOLDER, secure_filename(job.filename)))
+        resume.save(os.path.join(UPLOAD_FOLDER, secure_filename(resume.filename)))
+        job.save(os.path.join(UPLOAD_FOLDER, secure_filename(job.filename)))
 
-    return {"success": True}
+        # Logic
+
+        return jsonify(success=True, message="Files uploaded!")
+
+    except Exception as e:
+        print("UPLOAD ERROR:", e)
+        return jsonify(success=False, error=str(e)), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # Clean folder
